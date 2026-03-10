f21432d44c2ea1e03582836ad85e6d18ffc04949
asarritzu-boop <a.sarritzu@gmail.com>
2026-03-10 10:59:00 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rapporto-servizio-v2';
+const CACHE_NAME = 'rapporto-servizio-v3';
 const ASSETS = [
   './',
   './index.html',
@@ -6,33 +6,33 @@ const ASSETS = [
   './rapporti.png'
 ];
 
-// Installazione: scarica i file nella cache
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
 
-// Attivazione: pulisce le vecchie versioni della cache
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME)
-            .map((key) => caches.delete(key))
-      );
-    })
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
   );
+  self.clients.claim();
 });
 
-// Strategia di recupero: prova la rete, se offline usa la cache
+// Strategia: Cache first, fallback su rete
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).then((response) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    }).catch(() => caches.match('./index.html'))
   );
+});  );
 });
